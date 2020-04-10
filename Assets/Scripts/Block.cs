9be04f1cae22a7988ea1935f5de02aa9c173b815
Hex-Zero <hex0@live.com>
2020-04-10 16:40:31 +0100

    More blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(gameObject);
     }
 }
