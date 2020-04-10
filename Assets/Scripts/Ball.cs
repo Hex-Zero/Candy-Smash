e2cc7ede23d0c4e30abbfabc5d834f4875b804f1
Hex-Zero <hex0@live.com>
2020-04-10 15:04:25 +0100

    Wall collision and blocks

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
     }
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -16,7 +16,7 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mousePosInUnits = (Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
         paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
         transform.position = paddlePos;
